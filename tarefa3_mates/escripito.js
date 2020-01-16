2bb25462759bbb9d525183959a429e2748a3d26c
IagoLMSMR2 <56600595+IagoLMSMR2@users.noreply.github.com>
2020-01-16 18:11:31 +0100

    Add files via upload

---
+++ tarefa3_mates/escripito.js
@@ -0,0 +1,18 @@
+var primera = 5.24;
+var segunda = 6.19;
+var tercera = 7.10;
+
+var uno = Math.sqrt(primera)
+var dos = Math.pow(segunda, 2)
+var tres = Math.PI
+var tresDos = Math.round(tres)
+var cuatro = Math.min(primera, segunda, tercera)
+var cinco = Math.random(0, 3)
+
+
+
+alert(uno)
+alert(dos)
+alert(tresDos)
+alert(cuatro)
+alert(cinco)
