e32acd067b7241be5974d2ecf55d40a15ae7d50d
IagoLMSMR2 <56600595+IagoLMSMR2@users.noreply.github.com>
2020-01-16 18:24:14 +0100

    Add files via upload

---
+++ tarefa5_ducias2ovos/escripito.js
@@ -0,0 +1,6 @@
+var ovos = prompt("Cantas ducias de ovos leva unha tortilla");
+var duciasDeOvos = parseInt(ovos);
+var duciaDeOvos = duciasDeOvos * 12;
+ 
+alert(duciasDeOvos);
+alert(duciaDeOvos);
