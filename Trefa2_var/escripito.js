c3c84863aa0224c9bd8c02da1c68a16ceffade06
IagoLMSMR2 <56600595+IagoLMSMR2@users.noreply.github.com>
2020-01-16 18:02:41 +0100

    Add files via upload

---
+++ Trefa2_var/escripito.js
@@ -0,0 +1,12 @@
+var gastoHoxe = 22.22;
+var gastoOnte = 33.33;
+var uno = 58534.351;
+var dos = 512.37
+var total = gastoHoxe + gastoOnte;
+var resultado = gastoHoxe * gastoOnte;
+var muchosDecimales = uno / dos
+
+
+alert(muchosDecimales)
+alert(resultado)
+alert(total);
